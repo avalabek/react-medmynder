ae48e1ae71013dfb22e734cb7a7a7beb5dc263f0
Andreya <avalabek@yahoo.com>
2018-04-04 22:42:56 -0700

    fix app.js typos

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+import Landing from './pages/Landing';
+import Books from './pages/Books'
 
 class App extends Component {
   render() {
     return (
+      // <Router>
       <div className="App">
         <Nav />
+          {/* <Route exact path="/" component={Landing} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/profile" component={Books} /> */}
         <Signup />
           
       </div>
+      // </Router>
     );
   }
 }

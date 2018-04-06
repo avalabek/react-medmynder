63c79b8ee2e62306dfe6bdbd1799816c66e186f4
Ravi <rsavaj@gmail.com>
2018-04-05 17:35:39 -0700

    render Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import './App.css';
-
+import Nav from "./components/Nav";
+ 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         
           
       </div>

0ea82fc0c7a8fbd2f339b67be1963bfcc8f04560
Andreya <avalabek@yahoo.com>
2018-04-06 16:12:05 -0700

    radio buttons in column on card

--- client/src/components/RadioButtons/RadioButtons.js
+++ client/src/components/RadioButtons/RadioButtons.js
@@ -4,26 +4,28 @@ import React from "react";
 
 export const RadioButtons = props => (
   <form action="#">
-    <div className="container">
-    <p>
+    {/* <div className="container"> */}
+    <div className="row">
+      <span>
       <label>
         <input name="group1" value="1" type="radio" checked />
         <span>Once a day</span>
       </label>
-    </p>
-    <p>
+    
+    
       <label>
         <input name="group1" value="2" type="radio" />
         <span>Twice a day</span>
       </label>
-    </p>
-    <p>
+    
+    
       <label>
         <input class="with-gap" value="3" name="group1" type="radio" />
-        <span>Three times a day</span>
+        <span>Three times</span>
       </label>
-    </p>
+      </span>
     </div>
+    {/* </div> */}
   </form>
 
 

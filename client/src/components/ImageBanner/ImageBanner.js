97a2cdb7523411951eafcd3c02718d2f2f7bece4
Miriam Khaled <mkhaled82@gmail.com>
2018-04-10 14:10:05 -0700

    Landing Page

---
+++ client/src/components/ImageBanner/ImageBanner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ImageBanner.css";
+
+const ImageBanner = ({ children }) => (
+
+	<div className="section no-pad-bot" id="index-banner">
+	    <div className="container" id="banner">
+	      <h1 className="logo"> MedMynder </h1> <br />
+	      
+	     </div>
+
+	 
+
+	     <div className="container" id="banner2">
+	        <h5 className="logo2">Managing Your Medication has Never Been Easier.
+	        </h5>
+      	</div>
+	  </div>
+ );
+
+
+export default ImageBanner;

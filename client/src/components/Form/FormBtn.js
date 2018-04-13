da0dc120352b4c395577aa73c0cb9fa98a49e31f
Andreya <avalabek@yahoo.com>
2018-04-13 16:13:40 -0700

    fix merge conflicts

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,17 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-
-export const FormBtn = props => (
-  <button 
-  {...props} 
-  className="waves-effect waves-light btn"
-  type="submit"
-  name="action"
-  style={{ fontFamily: "IBM Plex Sans" }} 
-  message="message" >
-    {props.children}
-  </button>
-=======
 import { Link } from "react-router-dom";
 
 export const FormBtn = props => (
@@ -29,5 +16,4 @@ export const FormBtn = props => (
       </button>
     </Link>
   </div>
->>>>>>> 02853fe145073dc6848721677d67dc97393d42d2
 );
--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -7,11 +7,7 @@ import "./SignIn.css";
 
 
 
-<<<<<<< HEAD
-class SignIn extends React.Component {
-=======
 class SignIn extends Component {
->>>>>>> 02853fe145073dc6848721677d67dc97393d42d2
   constructor(props) {
     super(props);
     this.state = {
@@ -42,87 +38,11 @@ class SignIn extends Component {
   };
   
 
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> 02853fe145073dc6848721677d67dc97393d42d2
   render() {
     return (
      
       <div className="container" >
 
-<<<<<<< HEAD
-        <br /> <br />
-         <div className="card" >
-            <div className="card-content" >
-        <form >
-          <div className="row" >
-          
-            <div className="col s4" />
-              <div className="col s5" >
-              <img src={"../images/loginicon.png"} />
-         
-              <h5 className="header-center"> Log In </h5>
-            
-              <Input
-                value={this.state.username}
-                onChange={this.handleInputChange.bind(this)}
-                name="username"
-                placeholder="Username"
-              />
-            
-
-           
-              <Input
-                value={this.state.password}
-                onChange={this.handleInputChange.bind(this)}
-                name="password"
-                placeholder="Password"
-              />
-             
-        
-              
-            
-              <FormBtn
-                
-                onClick={this.handleFormSubmit}
-              >
-               Sign In
-              </FormBtn>
-          <br /> <br /> 
-
-            <h5 className="header-center"> New User? Register Here </h5>
-               <FormBtn
-              >
-            Register
-              </FormBtn>
-
-
-
-
-              </div>
-              </div>
-              
-
-            </form>
-            </div>
-              </div>
-          
-            </div>
-
-
- 
-
-          
- 
-
-        
-
-        
-=======
       <br /> <br />
     
       <form >
@@ -171,13 +91,8 @@ class SignIn extends Component {
      
 
       </div>   
->>>>>>> 02853fe145073dc6848721677d67dc97393d42d2
     );
   }
 }
 
-<<<<<<< HEAD
 export default SignIn;
-=======
-export default SignIn;
->>>>>>> 02853fe145073dc6848721677d67dc97393d42d2

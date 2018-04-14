879816b95e81f09574f1e1a88ac98532c7cef2e6
Miriam Khaled <mkhaled82@gmail.com>
2018-04-13 19:13:51 -0700

    rendering meds on page

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -7,7 +7,7 @@ import TableData from "../../components/TableData";
 //does that mean that it should only be in profile page, but how can the child(table)
 // access the state from the user inputs.
 
-class Table extends Component {
+ class Table extends Component {
   constructor(props) {
     super(props);
     console.log("props", props); //is empty string
@@ -16,121 +16,36 @@ class Table extends Component {
 
     };
   }
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     medicine: "",
-  //     for: "",
-  //     dosage: "",
-  //     frequency: "",
-  //     notes: ""
 
-  //   };
-  // }
-  // handleInputChange = event => {
-  //   const { name, value } = event.target;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
-  //commented out code at bottom of file may go here??
-
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   this.setState({
-  //     medicine: "",
-  //     indication: "",
-  //     dosage: "",
-  //     frequency: "",
-  //     notes: ""
-  //   });
-  //   alert("Form submitted.")
-  // };
 
   render() {
-    return <div>
-        <br />
-        <table className="responsive-table">
-          <thead>
-            <tr>
-              <th>Medicine</th>
-              <th>Indication</th>
-              <th>Dosage</th>
-              <th>Frequency</th>
-              <th>Notes</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
+    return (
 
-          <tbody>
-            <tr>
-              <td medicines={this.state.medicines}>{this.state.medicines}</td>
-              <td>blood pressure</td>
-              <td>1</td>
-              <td>twice</td>
-              <td>blue</td>
-              <DeleteButton />
-
-            </tr>
-            </tbody>
-            {/* <tr>
-              <TableData> {this.props.indication} </TableData>
+    <div className = "col m6 s6">
+           
+          
+             <ul>
+                {this.props.medicines.map(meds => (
+                  <li key={meds._id}>
+                  
+                      <strong>
+                      
+                        {meds.medicine} 
+                      </strong>
+                 
+                
+                  </li>
+                ))}
               
-            </tr> */}
-          
-            <tr>
-              <TableData 
-                medicines={this.props.medicines}
-                value={this.props.dosage} 
-                onChange={this.props.handleInputChange} 
-               
-              />
-            </tr>
-          
-            {/* <DeleteButton
-              // onClick={() => this.deleteMedicine(medicine._id)} 
-              /> */}
-<tbody>
-            <tr>
-              <td>Ciprofolxacin</td>
-              <td>antibiotic</td>
-              <td>strep throat</td>
-              <td>three times</td>
-              <td>upsets my stomach</td>
-              {/* <DeleteButton
-              // onClick={() => this.deleteMedicine(medicine._id)} 
-              /> */}
-            </tr>
-          </tbody>
-        </table>
-      </div>;
-  }
+              </ul>
+           
+             
+         
+
+      </div>
+      );
 }
+}
+
+
 export default Table;
-// does the below go here or in profile page component?
-  // When the component mounts, load all meds 
-  // componentDidMount() {
-  //   this.loadMeds();
-  // }
-
-  // // Loads all meds and sets them to this.state.meds
-  // loadMeds = () => {
-  //   API.getMeds()
-  //     .then(res =>
-  //       this.setState({ 
-  //   medicine: res.data, 
-  //   for: "", 
-  //   dosage: "", 
-  //   frequency: "",
-        // notes: ""
-  //  })
-  //     )
-  //     .catch(err => console.log(err));
-  // };
-
-  // // Deletes a med from the database with a given id, then reloads books from the db
-  // deleteMed = id => {
-  //   API.deleteMed(id)
-  //     .then(res => this.loadMeds())
-  //     .catch(err => console.log(err));
-  // };

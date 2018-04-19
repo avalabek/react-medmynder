29329f97ba4b1689575ae7704350b2152a7dafb8
Miriam Khaled <mkhaled82@gmail.com>
2018-04-19 15:37:42 -0700

    changed landing page

--- client/src/components/ImageBanner/ImageBanner.js
+++ client/src/components/ImageBanner/ImageBanner.js
@@ -5,7 +5,7 @@ const ImageBanner = ({ children }) => (
 
 	<div className="section no-pad-bot" id="index-banner">
 	    <div className="container" id="banner">
-	      <h1 className="logo"> MedMynder  </h1> 
+	      <h1 className="logo"> MedMynder  <img src={require("../../images/cell .png")} alt={""} id="cellicon"/> </h1> 
 	    </div>
 	</div>
  );
